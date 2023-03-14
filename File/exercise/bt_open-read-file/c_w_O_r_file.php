7d5a115b96ec93687f888655bbe8245b1f3dac2d
thibackend <thi.a24technology@gmail.com>
2023-03-15 02:14:03 +0700

    modified and grow file_w_r-format

--- File/exercise/bt_open-read-file/c_w_O_r_file.php
+++ File/exercise/bt_open-read-file/c_w_O_r_file.php
@@ -2,5 +2,8 @@
 <?php 
 
  echo nl2br(file_get_contents('./thi.txt',false,stream_context_create(),1));
+ function thi(){
+    return "thi handsome";
 
+ }
 ?>
--- File/exercise/file_write_read-format/index.php
+++ File/exercise/file_write_read-format/index.php
@@ -1,6 +1,48 @@
 <html>
     
     <body>
-        
+        <form action="index.php" method="post" name="frm" class="frm">
+
+            <label for="namefile">
+                namefile : <input type="text" name="namefile" id="namefile">
+            </label><br>
+            <label for="txt_content">
+                TextContent : <textarea name="txt_content" id="txt_content" cols="30" rows="10">
+                </textarea>
+            </label><br>
+            <br>
+            <input type="submit" value="Tao" name="Tao">
+        </form>
     </body>
 </html>
+
+<?php 
+if(isset($_POST["Tao"])){
+    if(!empty($_POST["namefile"]) && !empty($_POST['txt_content'])){
+        $namefile =$_POST['namefile'];
+        $content =$_POST['txt_content'];
+        // $file=fopen("$namefile","w");
+        // fwrite($file,$content);
+        // echo "successful write file";
+        // fclose($file);
+        echo "the file you want to create is : "."
+        $namefile <br> and content we have: $content <br> <br>";
+
+        if(file_exists('huu.txt')){
+            $content=fopen('./huu.txt',"r");
+            while(!feof($content)){
+                echo nl2br(fgets($content));
+            }
+            echo " <br> <br>the file existed";
+            fclose($content);
+        }else{
+            echo "this file doesn't exist";
+        }
+        
+        // while(!feof($content)){
+        //     echo nl2br($content);
+        // }
+    }
+}
+?>
+
